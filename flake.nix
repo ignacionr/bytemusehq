cbc53c2e627c58d7d961eed247b4bb18cb3be83b
Ignacio Rodriguez <ignacionr@gmail.com>
2026-02-11 17:32:35 -0300

    Bump version to 1.0.44

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "bytemusehq";
-          version = "1.0.43";
+          version = "1.0.44";
 
           src = ./.;
 
